b3e71dadf9c05dabef6417ed11ee9446da1f6bf7
Marekus <marek.stransky1@gmail.com>
2022-04-08 12:35:23 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
-window.onload(function() {
-    function ahref() {
-      $("#carouselDownSection").show();
-    };
-    setTimeout(function(){ ahref(); }, 3000);
-})
+window.onload = function(){
+  setTimeout(function(){
+    $("#carouselDownSection").show();
+  }, 3000);
+};
 
-window.onload(function() {
-    function ahref() {
-      $("#home-block-software-company").show();
-    };
-    setTimeout(function(){ ahref(); }, 3000);
-})
+window.onload = function(){
+  setTimeout(function(){
+    $("#home-block-software-company").show();
+  }, 3000);
+};

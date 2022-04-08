a9f73c312699bfab72f50d42db9e4287b2ce176d
Marekus <marek.stransky1@gmail.com>
2022-04-08 12:12:33 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+    $(".carouselDownSection").delay(2000).fadeIn(500);
+});
+
+$(document).ready(function() {
+    $(".home-block-software-company").delay(2000).fadeIn(500);
+});
